05e84927a50d93cd26ebd821f81af9d522cd9b5c
zulhamdani23 <zulhamdani.mb@gmail.com>
2022-09-21 10:40:43 +0700

    init commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+import {Sequelize} from "sequelize";
+
+var db = new Sequelize(
+    process.env.DB_SCEM, 
+    process.env.DB_USER, 
+    process.env.DB_PASS, {
+	dialect: process.env.DB_DIALECT,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	logging : false,
+	pool: {
+	    max: 5,
+	    min: 2,
+	    idle: 10000,
+	    acquire: 30000
+  	}
+});
+
+module.exports = db;
+  
